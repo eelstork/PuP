ed63a9f4e840099620eae2df5017e463d768877b
tea <tea.desouza@gmail.com>
2022-07-07 04:47:16 +0800

    Add changes from latest version

--- Assets/PuP/Editor/PuP.cs
+++ Assets/PuP/Editor/PuP.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 namespace Activ.Packaging{
 public class PuP : MonoBehaviour
@@ -14,6 +15,13 @@ public class PuP : MonoBehaviour
     static void Apply(){
         Debug.Log("Load requirements from requirements.txt");
         new LoadAction().Apply();
+    }
+
+    [MenuItem("Window/Activ/Pup/Restore Manifest")]
+    static void Restore(){
+        File.Delete("Packages/manifest.json");
+        File.Copy("Packages/manifest.json.backup",
+                  "Packages/manifest.json");
     }
 
 }}
