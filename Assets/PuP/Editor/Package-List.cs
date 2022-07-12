37f6217d36b8b345a6e6f73bae50cbf7bcc6d8d2
tea <tea.desouza@gmail.com>
2022-07-12 12:08:47 +0800

    Update common package list

--- Assets/PuP/Editor/Package-List.cs
+++ Assets/PuP/Editor/Package-List.cs
@@ -5,10 +5,9 @@ static readonly XPackageRef[] packages = { null,
 // =================================================================
 
 ("Activ-LT", "com.activ.stateless", "Behavior trees", "https://github.com/active-logic/active-lt.git"),
-("Debug-Chan", "com.active.debug-chan", "Frame logger", "https://github.com/eelstork/Debug-Chan.git"),
-("VRM", "com.vrmc.univrm", "Avatar System", "https://github.com/vrm-c/UniVRM.git?path=/Assets/VRM#v0.81.0"),
-//("SpringBone", "name???", "Desc?", "???"),
-//("XGoap", "name???", "AI Planner", "https://github.com/active-logic/xgoap.git")
+("Debug-Chan", "com.activ.debug-chan", "Frame logger", "https://github.com/eelstork/Debug-Chan.git"),
+("Unity Chan Spring Bone", "com.unity.springbone", "Spring Bone System", "https://github.com/unity3d-jp/UnityChanSpringBone.git"),
+//("UniVRM", "com.vrmc.univrm", "Avatar System", "https://github.com/vrm-c/UniVRM.git?path=/Assets/VRM#v0.101.0")
 
 // =================================================================
 
