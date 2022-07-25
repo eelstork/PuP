1ab656f4aeca4b6855d621130db81acd7418cda7
tea <tea.desouza@gmail.com>
2022-07-25 14:13:11 +0800

    Redirect debug chan version

--- Assets/PuP/Editor/Package-List.cs
+++ Assets/PuP/Editor/Package-List.cs
@@ -5,7 +5,7 @@ static readonly XPackageRef[] packages = { null,
 // =================================================================
 
 ("Activ-LT", "com.activ.stateless", "Behavior trees", "https://github.com/active-logic/active-lt.git"),
-("Debug-Chan", "com.activ.debug-chan", "Frame logger", "https://github.com/eelstork/Debug-Chan.git"),
+("Debug-Chan", "com.activ.debug-chan", "Frame logger", "https://github.com/active-logic/Debug-Chan.git"),
 ("Unity Chan Spring Bone", "com.unity.springbone", "Spring Bone System", "https://github.com/unity3d-jp/UnityChanSpringBone.git"),
 //("UniVRM", "com.vrmc.univrm", "Avatar System", "https://github.com/vrm-c/UniVRM.git?path=/Assets/VRM#v0.101.0")
 
